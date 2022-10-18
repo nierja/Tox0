2d516305d74fdc4aaff7e45a748f00e9b6f94d3f
Jakub Nierostek <jakub.nierostek@gmail.com>
2022-10-18 15:29:29 +0200

    Modification that makes sklearn models use CPU resources only.

--- train_all.sh
+++ train_all.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
-
-targets=("NR-AR" "NR-AR-LBD" "NR-AhR" "NR-Aromatase" "NR-ER" "NR-ER-LBD" "NR-PPAR-gamma" "SR-ARE" "SR-ATAD5" "SR-HSE" "SR-MMP" "SR-p53")
-# models=("most_frequent" "gbt" "lr" "svm" "adalr" "baglr" "badlr" "mlp", "tf_dnn")
-# targets=("NR-AR") # "NR-AR-LBD" "NR-AhR" "NR-Aromatase" "NR-ER" "NR-ER-LBD" "NR-PPAR-gamma" "SR-ARE" "SR-ATAD5" "SR-HSE" "SR-MMP" "SR-p53")
-models=("tf_dnn") # "gbt" "lr" "svm" "adalr" "baglr" "badlr" "mlp", "tf_dnn")
-
-
 cd /auto/brno2/home/nierja/classification/Tox
+targets=("NR-AR" "NR-AR-LBD" "NR-AhR" "NR-Aromatase" "NR-ER" "NR-ER-LBD" "NR-PPAR-gamma" "SR-ARE" "SR-ATAD5" "SR-HSE" "SR-MMP" "SR-p53")
 
+### CPU trained models
+models=("most_frequent" "gbt" "lr" "svm" "adalr" "baglr" "badlr" "mlp")
+for target in ${targets[@]}; do
+	for model in ${models[@]}; do
+  		qsub -v target=$target,model=$model cpu_job.sh
+	done
+done
+
+### GPU trained models
+models=("tf_dnn")
 for target in ${targets[@]}; do
 	for model in ${models[@]}; do
   		qsub -v target=$target,model=$model gpu_job.sh
 	done
 done
+
